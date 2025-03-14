55fec7620defe6f84d95cd62ff314f0589712499
Alessandro Feitoza <Alessandro.Feitoza@digitalcollege.internal>
2025-03-13 21:57:08 -0300

    consumindo api

--- js/produto.js
+++ js/produto.js
@@ -1,78 +1,65 @@
 const TABLE = document.getElementById('table-produto');
 
-let dados = [
+    fetch('http://localhost:3000/products')
+        .then (res => res.json())
+        .then (dados => {
+            dados.forEach((products)=> {
+                TABLE.innerHTML += `
+                    <tr>
+                        <td>${products.id}</td>
+                        <td>${products.name}</td>
+                        <td>${products.category}</td>
+                        <td> <img src="${products.image}" onclick="abrirModal('${products.name}' ,'${products.image}')" width="50px" data-bs-toggle="modal" data-bs-target="#exampleModal"></td>
+                        <td>${products.quantity}</td>
+                        <td>${products.price}</td>
+                        <td>
+                            <a href="#" class="btn btn-outline-warning btn-sm">
+                                Editar
+                            </a>
+            
+                            <a href="#" onclick = "excluir(${products.id})" class="btn btn-outline-danger btn-sm">
+                                Excluir
+                            </a>
+                        </td>
+                    </tr>
+            
+                `;
+            
+            
+            });
 
-    {
+            
 
-        id: 1,
-        nome: 'Vale Real',
-        categoria : 'Vinho',
-        imagem : 'https://phygital-files.mercafacil.com/catalogo/uploads/produto/vinho_vale_real_900ml_a4b3654b-d5ad-475c-9eda-6eaea8d339ff.jpeg',
-        quantidade: '7',
-        valor: '$5,00',
-    
-    },
+        
 
-
-    {
-
-        id: 1,
-        nome: 'São Braz',
-        categoria : 'Vinho',
-        imagem : 'https://deskontao.agilecdn.com.br/25537_1.jpg',
-        quantidade: '7',
-        valor: '$5,00',
-    
-    },
-
-
-    {
-
-        id: 1,
-        nome: 'São Bento',
-        categoria : 'Vinho',
-        imagem : 'https://cdn.awsli.com.br/1958/1958204/produto/203245775/screenshot_20230223_190651_instagram-pamtvr.jpg',
-        quantidade: '7',
-        valor: '$5,00',
-    
-    },
-
-];
-
-dados.forEach((produto)=> {
-
-    TABLE.innerHTML += `
-        <tr>
-            <td>${produto.id}</td>
-            <td>${produto.nome}</td>
-            <td>${produto.telefone}</td>
-            <td> <img src="${produto.imagem}" onclick="abrirModal('${produto.nome}' ,'${produto.imagem}')" width="50px" data-bs-toggle="modal" data-bs-target="#exampleModal"></td>
-            <td>${produto.quantidade}</td>
-            <td>${produto.valor}</td>
-            <td>
-                <a href="#" class="btn btn-outline-warning btn-sm">
-                    Editar
-                </a>
-
-                <a href="#" class="btn btn-outline-danger btn-sm">
-                    Excluir
-                </a>
-            </td>
-        </tr>
-
-    `;
+        });
 
 
-});
+    function abrirModal(nome, imagem){
+        document.getElementById('modal_produto_nome').innerHTML = nome;
+        document.getElementById('modal_produto_body').innerHTML = `
+        
+        <img src= "${imagem}" width="100%">
+        
+        `;
+    }
+
+    function excluir(id) {
+
+        if(false === confirm ('Confirma que deseja excluir o produto?')){
+            return;
+        }
 
 
-function abrirModal(nome, imagem){
-    document.getElementById('modal_produto_nome').innerHTML = nome;
-    document.getElementById('modal_produto_body').innerHTML = `
-    
-    <img src= "${imagem}" width="100%">
-    
-    `;
-}
+        fetch(`http://localhost:3000/products/${id}`, {
+            method: 'DELETE'
+
+
+        });
+
+        alert("Produto Excluído");
+        location.href = "";
+
+    }
 
 
